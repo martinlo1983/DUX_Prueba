50d7398c4ad53059311ef13769b07783e79917a6
martinlo1983 <m.lopez.ceter@gmail.com>
2026-05-06 17:38:12 -0300

    Update dux_api_demo_probe.py

--- dux_api_demo_probe.py
+++ dux_api_demo_probe.py
@@ -81,7 +81,7 @@ def make_headers() -> Dict[str, str]:
     # {"token": DUX_TOKEN, ...}
     # o {"X-API-Key": DUX_TOKEN, ...}
     return {
-        "token": "asd09",
+        "token": DUX_TOKEN,
         "Accept": "application/json",
         "Content-Type": "application/json",
     }
